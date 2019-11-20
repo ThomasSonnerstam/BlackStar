594dc00f0e080c7da12e46c2ea995fb671f67581
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-11-20 13:13:46 +0100

    Updates: - removed about.html - fixed navigation bug

--- script.js
+++ script.js
@@ -22,16 +22,21 @@ const navSlide = () => {
         burger.classList.toggle("toggle");
     });
 
-
-    // WHY DOESN'T THIS WORK? 
-
+    // Nav menu closes when clicking on a link
     navLinks.forEach(link => {
         link.addEventListener("click", () => {
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = "";
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+                }
+            });
             nav.classList.remove("nav-active");
-            link.style.animation = "";
         })
     });
 
+    // Burger goes from cross to burger when clicking on a link
     navLinks.forEach(link => {
         link.addEventListener("click", () => {
             burger.classList.toggle("toggle");
@@ -41,65 +46,3 @@ const navSlide = () => {
 }
 
 navSlide();
-
-// // Function for the carousel image slide show
-
-// const carouselSlide = document.querySelector(".carousel-slide");
-// const carouselImages = document.querySelectorAll(".carousel-slide img");
-// const prevButton = document.querySelector("#prev-button");
-// const nextButton = document.querySelector("#next-button");
-
-// // Counter
-
-// let counter = 1;
-
-// // Width of the images
-
-// const imageWidth = carouselImages[0].clientWidth;
-
-// // Start image slide on second picture (the first & last picture are duplicates)
-
-// carouselSlide.style.transform = "translateX(" + (-imageWidth * counter) + "px)";
-
-// // Transition into next slide when pressing the next button
-
-// nextButton.addEventListener("click", () => {
-
-//     if (counter >= carouselImages.length - 1) {
-//         return;
-//     }
-
-//     carouselSlide.style.transition = "transform 0.5s ease-in";
-//     counter++;
-//     carouselSlide.style.transform = "translateX(" + (-imageWidth * counter) + "px)";
-// });
-
-// prevButton.addEventListener("click", () => {
-
-//     if (counter <= 0) {
-//         return;
-//     }
-
-//     carouselSlide.style.transition = "transform 0.5s ease-in";
-//     counter--;
-//     carouselSlide.style.transform = "translateX(" + (-imageWidth * counter) + "px)";
-
-// });
-
-// carouselSlide.addEventListener("transitionend", () => {
-
-//     if (carouselImages[counter].id === "last-clone") {
-//         carouselSlide.style.transition = "none";
-//         counter = carouselImages.length - 2;
-//         carouselSlide.style.transform = "translateX(" + (-imageWidth * counter) + "px)";
-
-//     }
-
-//     if (carouselImages[counter].id === "first-clone") {
-//         carouselSlide.style.transition = "none";
-//         counter = carouselImages.length - counter;
-//         carouselSlide.style.transform = "translateX(" + (-imageWidth * counter) + "px)";
-
-//     }
-
-// });
